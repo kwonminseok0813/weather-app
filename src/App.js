7d0451bf63fd689b9e554efda779fdb597447d28
kwonminseok0813 <kwonminseok@gm.gist.ac.kr>
2024-08-18 19:05:58 +0900

    refactor:수정

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import React, { useState } from 'react';
 
 const api = {
@@ -20,44 +21,46 @@ function App() {
   }
 }
 
-const dateBuilder = (d) => {
-  let months = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
 
-  return `${day} ${date} ${month} ${year}`
-}
+
+
+
+
+
 
 return (
   <div className="App">
     <main>
-      <div className='Search-box'>
-        <input
-          type='text'
-          className='search-bar'
-          placeholder='search...'
-          onChange={e => setQuery(e.target.value)}
-          value={query}
-          onKeyDown={search}
-        />
-      </div>
-      {(typeof weather.main !== "undefined") ? (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          search(e);
+        }}
+      >
+        <div className='search-box'>
+          <input
+            type='text'
+            className='search-bar'
+            placeholder='search...'
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+          />
+        </div>
+      </form>
+      {weather.main !== undefined && (
         <div>
-          <div className='Date-box'>
+          <div className='date-box'>
             <div className='location'>{weather.name}, {weather.sys.country}</div>
-            <div className='date'>{dateBuilder(new Date())}</div>
+            <div className='date'>{dayjs().format("dddd-DD-MM-YYYY")}</div>
           </div>
-          <div className='Weather-box'>
+          <div className='weather-box'>
             <div className='temp'>{Math.round(weather.main.temp)}°C</div>
             <div className='weather'>{weather.weather[0].main}</div>
           </div>
         </div>
-      ) : ('')}
+      )}
+
 
     </main>
   </div>
